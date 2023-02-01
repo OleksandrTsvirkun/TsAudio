d33cc8dd15f4d23aa0b334e6577181f76c68b63b
Oleksandr Tsvirkun <alexandertsvirkun@gmail.com>
2023-02-01 18:25:01 +0200

    small method refactoring

--- src/TsAudio/Sample/PeekProviders/AveragePeakProvider.cs
+++ src/TsAudio/Sample/PeekProviders/AveragePeakProvider.cs
@@ -1,71 +1,82 @@
 ﻿using System;
 using System.Linq;
 using System.Numerics;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace TsAudio.Sample.PeekProviders
+namespace TsAudio.Sample.PeekProviders;
+
+public class AveragePeakProvider : PeakProvider
 {
-    public class AveragePeakProvider : PeakProvider
+    private readonly float scale;
+
+    public CancellationToken CancellationToken { get; set; }
+
+    public AveragePeakProvider(float scale = 1f)
     {
-        private readonly float scale;
+        this.scale = scale;
+    }
 
-        public CancellationToken CancellationToken { get; set; }
+    public override async ValueTask<bool> MoveNextAsync()
+    {
+        var buffer = this.BufferOwner.Memory.Slice(0, this.SamplesPerPeak);
+        var read = await this.Provider.ReadAsync(buffer, this.CancellationToken);
 
-        public AveragePeakProvider(float scale = 1f)
+        if (read == 0)
         {
-            this.scale = scale;
+            return false;
         }
 
-        public override async ValueTask<bool> MoveNextAsync()
+        this.Current = this.CalculatePeak(read);
+
+        return true;
+    }
+
+    private PeakInfo CalculatePeak(int count)
+    {
+        var span = this.BufferOwner.Memory.Span.Slice(0, count);
+
+        var left = 0.0f;
+        var right = 0.0f;
+        var length = span.Length / 2;
+
+        if(Vector.IsHardwareAccelerated)
         {
-            var buffer = this.BufferOwner.Memory.Slice(0, this.SamplesPerPeak);
-            var read = await this.Provider.ReadAsync(buffer, this.CancellationToken);
-
-            if (read == 0)
-            {
-                return false;
-            }
-
-            this.Current = this.CalculatePeak(read);
-
-            return true;
+            this.CalculatePeaksVectorized(span, out left, out right, length);
+        }
+        else
+        {
+            this.CalculatePeaksScalared(span, ref left, ref right, length);
         }
 
-        private PeakInfo CalculatePeak(int count)
+        return new PeakInfo(-right, left);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void CalculatePeaksScalared(Span<float> span, ref float left, ref float right, int length)
+    {
+        for(int i = 0; i < span.Length - 1;)
         {
-            var span = this.BufferOwner.Memory.Span.Slice(0, count);
-
-            var left = 0.0f;
-            var right = 0.0f;
-            var length = span.Length / 2;
-
-
-            if(Vector.IsHardwareAccelerated)
-            {
-                var sample = Vector4.Zero;
-
-                for(int i = 0; i < span.Length - 4;)
-                {
-                    sample += new Vector4(Math.Abs(span[i++]), Math.Abs(span[i++]), Math.Abs(span[i++]), Math.Abs(span[i++]));
-                }
-
-                right = this.scale * ((sample.Y + sample.W) / length);
-                left = this.scale * ((sample.X + sample.Z) / length);
-            }
-            else
-            {
-                for(int i = 0; i < span.Length - 1;)
-                {
-                    left += Math.Abs(span[i++]);
-                    right += Math.Abs(span[i++]);
-                }
-
-                right = this.scale * (right / length);
-                left = this.scale * (left / length);
-            }
-
-            return new PeakInfo(-right, left);
+            left += Math.Abs(span[i++]);
+            right += Math.Abs(span[i++]);
         }
+
+        right = this.scale * (right / length);
+        left = this.scale * (left / length);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void CalculatePeaksVectorized(Span<float> span, out float left, out float right, int length)
+    {
+        var sample = Vector4.Zero;
+
+        for(int i = 0; i < span.Length - 4;)
+        {
+            sample += new Vector4(Math.Abs(span[i++]), Math.Abs(span[i++]), Math.Abs(span[i++]), Math.Abs(span[i++]));
+        }
+
+        right = this.scale * ((sample.Y + sample.W) / length);
+        left = this.scale * ((sample.X + sample.Z) / length);
     }
 }
