81fe732c612d361bfc6a9cd5877a7bd905ecc3a2
Oleksandr Tsvirkun <alexandertsvirkun@gmail.com>
2023-02-01 18:16:37 +0200

    Change CacheStream to BufferedStreamManager

---
+++ src/TsAudio/Utils/Streams/BufferedStreamManagerReaderArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO.MemoryMappedFiles;
+using System.Threading;
+
+namespace TsAudio.Utils.Streams;
+
+internal class BufferedStreamManagerReaderArgs
+{
+    public ManualResetEventSlim ReadAwaiter { get; init; }
+
+    public MemoryMappedViewStream Reader { get; init; }
+
+    public Func<long> GetBuffered { get; init; }
+
+    public Func<bool> GetWritingIsDone { get; init; }
+
+    public Action<long> SetAdvance { get; init; }
+}
---
+++ src/TsAudio/Utils/Streams/BufferingOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TsAudio.Utils.Streams;
+
+public class BufferingOptions
+{
+    public int PauseWriterThreshold { get; init; }
+
+    public int ResumeWriterThreshold { get; init; }
+}
